38c8e2aa5b41bd4a189dd28e97bf083100ff793b
Deepika Namboo3 <31755347+deepikanambo3@users.noreply.github.com>
2018-10-02 13:47:22 -0400

    trial commit
    changed color to red from white

--- app/server.R
+++ app/server.R
@@ -3,7 +3,7 @@
 shinyServer(function(input, output) {
   
   output$distPlot <- renderPlot({
-    hist(rnorm(input$obs), col = 'darkgray', border = 'white')})
+    hist(rnorm(input$obs), col = 'darkgray', border = 'red')})
 })
   
   
