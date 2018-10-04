861f282be6eba626e7254a7538676fd3592d3f8d
Alex <yc3528@columbia.edu>
2018-10-04 15:45:00 -0400

    Attractions sidebar

--- app/ui.r
+++ app/ui.r
@@ -11,6 +11,27 @@ shinyUI(
     dashboardSidebar(
       sidebarMenu(
         menuItem("NYC Map", tabName = "mapmenu", icon = icon("bank")),
+        
+        # Attractions selection
+        selectizeInput('foo', label = h3("Attractions"),
+                       choices = list(
+                         `Statue of Liberty` = 'SL', `Central Park` = 'CP',
+                         `Rockefeller Center` = 'RC', `Metropolitan Museum of Art` = 'MA',
+                         `Broadway and Theater District` = 'BT', `Empire State Building` = 'ES',
+                         `9/11 Memorial and Museum` = '911', `The High Line` = 'HL',
+                         `Times Square` = 'TS', `Brooklyn Bridge` = 'BB',
+                         `Grand Central Terminal` = 'GS', `One World Observatory` = 'OW',
+                         `New York Public Library` = 'PL', `Charging Bull` = 'CB',
+                         `Radio City Music Hall` = 'RC', `St. Patrick's Cathedral` = 'SP',
+                         `Carnegie Hall` = 'CH', `Bryant Park` = 'BP',
+                         `Lincoln Center` = 'LC', `The Oculus` = 'O',
+                         `Macy's Herald Square` = 'MH'
+                       ),
+                       multiple = TRUE,
+                       options = list(maxItems = 3, placeholder = 'Choose at most 3 attractions')
+        ),
+        
+        # Radius slider
         sliderInput("decimal", "Distance (in miles):",
                     min = 0, max = 2.5,
                     value = 1.0, step=0.5),
@@ -18,26 +39,7 @@ shinyUI(
         # checkbox for price
         checkboxGroupInput("checkGroup", label = h3("Price"), 
                            choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3),
-                           selected = 1),
-        
-        # Attractions selection
-        selectizeInput('foo', label = h3("Attractions"),
-                       choices = list(
-                         c(`Statue of Liberty` = 'SL', `Central Park` = 'CP',
-                           `Rockefeller Center` = 'RC', `Metropolitan Museum of Art` = 'MA',
-                           `Broadway and Theater District` = 'BT', `Empire State Building` = 'ES',
-                           `9/11 Memorial and Museum` = '911', `The High Line` = 'HL',
-                           `Times Square` = 'TS', `Brooklyn Bridge` = 'BB',
-                           `Grand Central Terminal` = 'GS', `One World Observatory` = 'OW',
-                           `New York Public Library` = 'PL', `Charging Bull` = 'CB',
-                           `Radio City Music Hall` = 'RC', `St. Patrick's Cathedral` = 'SP',
-                           `Carnegie Hall` = 'CH', `Bryant Park` = 'BP',
-                           `Lincoln Center` = 'LC', `Lincoln Center` = 'O',
-                           `Macy's Herald Square` = 'MH'
-                         )),
-                       multiple = TRUE,
-                       options = list(maxItems = 3, placeholder = 'Choose at most 3 attractions')
-        )
+                           selected = 1)
         
         ) # End of sidebarMenu
     ), # End of Sidebar
